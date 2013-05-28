d8612b50a3a2f348ec8f5355080a8c0f16cf2467
David Marrs <david@davidwmarrs.net>
2013-05-28 19:56:32 +0100

    Adding Vagrant Puppet provisioner, with modules from Puppet Lab's Puppet Forge

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 Vagrant-Puppet-LAMP
 ===================
+
+Puppet provisioner for a Linux (Ubuntu Lucid Lynx 32-bit), Apache, MySQL & PHP (5) stack.
+
+Constructed using Puppet Lab's own modules from the Puppet Forge:
+
+apache - http://forge.puppetlabs.com/puppetlabs/apache - version 0.4.0
+firewall - http://forge.puppetlabs.com/puppetlabs/firewall - version 0.0.4
+mysql - http://forge.puppetlabs.com/puppetlabs/mysql - version 0.5.0
+stdlib - http://forge.puppetlabs.com/puppetlabs/stdlib - version 3.1.1
+
+Each module is within this repository as a git submodule, checked out at the relevant version tag. Newer versions are available but have not yet been tested in this project.
---
+++ manifests/default.pp
@@ -0,0 +1,84 @@
+# It would be preferable if we only had to run apt-get update when a new
+# package needed to be installed
+exec { "apt-update":
+  command => "/usr/bin/apt-get update",
+}
+
+Exec['apt-update'] -> Package <| |>
+
+include apache
+
+apache::vhost {'vagrant':
+  vhost_name => '*',
+  port => '80',
+  docroot => '/vagrant/www/',
+  override => 'All'
+}
+
+a2mod { 'rewrite' :
+  ensure => present,
+  notify => Service['httpd']
+}
+
+class {'apache::mod::php': 
+  notify => Service['httpd']
+}
+
+include mysql
+
+class { 'mysql::server':
+  config_hash => { 
+    'root_password' => 'Password01',
+    'bind_address' => false
+  }
+}
+
+database_user { 'root@10.0.2.2':
+  password_hash => mysql_password('Password01')
+}
+
+database_grant { 'root@10.0.2.2' :
+  privileges => ['all']
+}
+
+package { 'php5-mysql' :
+  ensure => installed,
+  require => Class['apache::mod::php'],
+  notify => Service['httpd'],
+}
+
+# The following packages and exec are required for Drupal 7
+package { 'php5-gd' :
+  ensure => installed,
+  require => Class['apache::mod::php'],
+  notify => Service['httpd'],
+}
+
+package { 'php-pear' :
+  ensure => installed,
+  require => Class['apache::mod::php'],
+}
+
+package { 'php5-dev' :
+  ensure => installed,
+  require => Class['apache::mod::php'],
+}
+
+package { 'make' :
+  ensure => installed,
+}
+
+exec { "pecl-uploadprogress":
+  command => "/usr/bin/pecl install uploadprogress",
+  require => [Package['php-pear'],Package['php5-dev'],Package['make']],
+  creates => "/usr/share/doc/php5-common/PEAR/uploadprogress"
+}
+
+# The new file for php.ini changes error reporting settings for a development
+# environment and adds the uploadprogress pecl extension (see below) 
+file {'/etc/php5/apache2/php.ini':
+  ensure => file,
+  require => Exec['pecl-uploadprogress'],
+  content => template('/vagrant/templates/php.ini.erb'),
+  notify => Service['httpd']
+}
